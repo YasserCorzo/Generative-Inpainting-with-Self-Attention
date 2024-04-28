de492f64f039fda2c2d68166b7adfd6fdfb16b23
Eric Yang <ericyang8484@gmail.com>
2024-04-27 23:47:25 -0400

    debugging generator and discriminator after combining code

--- model.py
+++ model.py
@@ -5,11 +5,11 @@ from layers import GatedConv, ResizeGatedConv, SpectralNormConv
 
 
 class Discriminator(nn.Module):
-    def __init__(self, in_channels):
+    def __init__(self):
         super(Discriminator, self).__init__()
 
         self.layers = nn.Sequential(
-            SpectralNormConv(in_channels, out_channels=64, kernel_size=5, stride=2, padding=2), # batch_size x 4 x 256 x 256 ---> batch_size x 64 x 128 x 128
+            SpectralNormConv(in_channels=4, out_channels=64, kernel_size=5, stride=2, padding=2), # batch_size x 4 x 256 x 256 ---> batch_size x 64 x 128 x 128
             SpectralNormConv(in_channels=64, out_channels=128, kernel_size=5, stride=2, padding=2), # batch_size x 128 x 64 x 64
             SpectralNormConv(in_channels=128, out_channels=256, kernel_size=5, stride=2, padding=2), # batch_size x 256 x 32 x 32
             SpectralNormConv(in_channels=256, out_channels=256, kernel_size=5, stride=2, padding=2), # batch_size x 256 x 16 x 16
@@ -37,11 +37,11 @@ class Discriminator(nn.Module):
 
 
 class Generator(nn.Module):
-    def __init__(self, in_channels):
+    def __init__(self):
         super(Generator, self).__init__()
 
         self.coarse_network = nn.Sequential(
-            GatedConv(in_channels, out_channels=32, kernel_size=5, stride=1, padding=2), # batch_size x 32 x 256 x 256
+            GatedConv(in_channels=4, out_channels=32, kernel_size=5, stride=1, padding=2), # batch_size x 32 x 256 x 256
             GatedConv(in_channels=32, out_channels=64, kernel_size=3, stride=2, padding=1), # batch_size x 64 x 128 x 128
             GatedConv(in_channels=64, out_channels=64, kernel_size=3, stride=1, padding=1), # batch_size x 64 x 128 x 128
             GatedConv(in_channels=64, out_channels=128, kernel_size=3, stride=2, padding=1), # batch_size x 128 x 64 x 64
@@ -61,14 +61,14 @@ class Generator(nn.Module):
         )
 
         self.refinement_network = nn.Sequential(
-            GatedConv(in_channels, out_channels=32, kernel_size=5, stride=1, padding=2), # batch_size x 32 x 256 x 256
+            GatedConv(in_channels=4, out_channels=32, kernel_size=5, stride=1, padding=2), # batch_size x 32 x 256 x 256
             GatedConv(in_channels=32, out_channels=32, kernel_size=3, stride=2, padding=1), # batch_size x 32 x 128 x 128
             GatedConv(in_channels=32, out_channels=64, kernel_size=3, stride=1, padding=1), # batch_size x 64 x 128 x 128
             GatedConv(in_channels=64, out_channels=128, kernel_size=3, stride=2, padding=1), # batch_size x 128 x 64 x 64
             GatedConv(in_channels=128, out_channels=128, kernel_size=3, stride=1, padding=1), # batch_size x 128 x 64 x 64
             GatedConv(in_channels=128, out_channels=128, kernel_size=3, stride=1, padding=1), # batch_size x 128 x 64 x 64
 
-            SelfAttention(in_channels=128),   # need to check this layer                      # batch_size x 128 x 64 x 64
+            SelfAttention(in_dim=128),   # need to check this layer                      # batch_size x 128 x 64 x 64
 
             GatedConv(in_channels=128, out_channels=128, kernel_size=3, stride=1, padding=1), # batch_size x 128 x 64 x 64
             GatedConv(in_channels=128, out_channels=128, kernel_size=3, stride=1, padding=1), # batch_size x 128 x 64 x 64
@@ -102,7 +102,7 @@ class Generator(nn.Module):
         # coarse network
         coarse_out = self.coarse_network(input)
         # clip output so values are between -1 and 1
-        coarse_clip = torch.clamp(coarse_out, -1, 1)
+        coarse_clip = torch.clamp(coarse_out, -1.0, 1.0)
 
         # return clipped output of coarse network
         # return coarse_clip
@@ -115,7 +115,7 @@ class Generator(nn.Module):
         # refinement network
         refine_in = torch.cat([coarse_processed, masks], dim=1)
         refine_out = self.refinement_network(refine_in)
-        refine_clip = torch.clamp(refine_out, -1, 1)
+        refine_clip = torch.clamp(refine_out, -1.0, 1.0)
 
         refine_raw = refine_clip
 
@@ -165,7 +165,7 @@ class Generator(nn.Module):
         '''
         return (-1) * torch.mean(x_hat)
 
-
+'''
 class SelfAttention(nn.Module):
 
     def __init__(self, in_channels: int = 3,
@@ -225,4 +225,41 @@ class SelfAttention(nn.Module):
         output = self.gamma * output + x
 
         return output, attention
+'''
 
+class SelfAttention(nn.Module):
+    """ Self attention Layer"""
+    def __init__(self,in_dim=128,activation='relu',with_attn=False):
+        super(SelfAttention,self).__init__()
+        self.chanel_in = in_dim
+        self.activation = activation
+        self.with_attn = with_attn
+        self.query_conv = nn.Conv2d(in_channels = in_dim , out_channels = in_dim//8 , kernel_size= 1)
+        self.key_conv = nn.Conv2d(in_channels = in_dim , out_channels = in_dim//8 , kernel_size= 1)
+        self.value_conv = nn.Conv2d(in_channels = in_dim , out_channels = in_dim , kernel_size= 1)
+        self.gamma = nn.Parameter(torch.zeros(1))
+
+        self.softmax  = nn.Softmax(dim=-1) #
+    def forward(self,x):
+        """
+            inputs :
+                x : input feature maps( B X C X W X H)
+            returns :
+                out : self attention value + input feature
+                attention: B X N X N (N is Width*Height)
+        """
+        m_batchsize,C,width ,height = x.size()
+        proj_query  = self.query_conv(x).view(m_batchsize,-1,width*height).permute(0,2,1) # B X CX(N)
+        proj_key =  self.key_conv(x).view(m_batchsize,-1,width*height) # B X C x (*W*H)
+        energy =  torch.bmm(proj_query,proj_key) # transpose check
+        attention = self.softmax(energy) # BX (N) X (N)
+        proj_value = self.value_conv(x).view(m_batchsize,-1,width*height) # B X C X N
+
+        out = torch.bmm(proj_value,attention.permute(0,2,1) )
+        out = out.view(m_batchsize,C,width,height)
+
+        out = self.gamma*out + x
+        if self.with_attn:
+            return out ,attention
+        else:
+            return out
